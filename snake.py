a4fa59b1ffbad0067197a38623e96d2ce9a8f6a0
Carter Luck <cartervluck@gmail.com>
2020-05-05 11:39:02 -0700

    set up printBoard

--- snake.py
+++ snake.py
@@ -9,3 +9,18 @@ import pynput #Library 'pynput' by Moses Palmer
 #START CODE BLOCK: CARTER LUCK
 board = [['' for i in range(10)] for j in range(10)] #10 by 10 matrix of ''. Backwards indexing: board[y][x] to index board (just for ease of display)
 fruitLocation = (0,0) #tuple for location of the fruit. (x,y)
+
+def printBoard():
+	print("|---------------------|")
+	for i in range(len(board)):
+		printstring = ["|"]
+		for j in range(len(board[i])):
+			if fruitLocation == (j,i):
+				printstring.append("O")
+			else: #TODO: PRINT X IF PART OF SNAKE IS HERE
+				printstring.append(" ")
+		printstring.append("|")
+		printstring = " ".join(printstring)
+		print(printstring)
+	print("|---------------------|")
+printBoard() #just to test
