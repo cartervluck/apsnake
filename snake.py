a66ffdfb2e6e110a7dada14438c761414392cea2
Carter Luck <cartervluck@gmail.com>
2020-05-05 11:24:30 -0700

    add crediting standards

--- snake.py
+++ snake.py
@@ -1,1 +1,6 @@
-
+#Written by Carter Luck and David Carlip.
+#All sections of code are written by the author of the code block unless specifically otherwise stated.
+#Code blocks are specified in comments like so:
+#START CODE BLOCK: CARTER LUCK
+#{code}
+#END CODE BLOCK
